2497bc86a4ea7cdcb66da63cc73a74da8b1eec48
Charmy Jain <charmyjain1@gmail.com>
2025-03-19 16:57:38 +0530

    last commit

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -1,13 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-	console.log("Cookies: ", req.cookies); // Debugging
 	const token = req.cookies.token;
-	console.log("Extracted Token:", token); // Debugging
 	if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-		console.log("Decoded Token:", decoded); // Debugging
 
 		if (!decoded) return res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
 
